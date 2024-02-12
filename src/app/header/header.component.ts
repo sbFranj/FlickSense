1f593f0a9663234097bd691a194c908eeabebd95
Fran Sánchez <115450163+sbFranj@users.noreply.github.com>
2024-02-12 19:40:37 +0100

    preparing project

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [SearchComponent],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
